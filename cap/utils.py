2b580fb1bb449e5ca72e3d7485e21b5072f48108
Yinghao-Li <www6com2010@hotmail.com>
2022-05-09 12:10:56 -0700

    update helper function

--- cap/utils.py
+++ cap/utils.py
@@ -2,6 +2,9 @@ import os
 import glob
 import json
 
+from seqlbtoolkit.text import substring_mapping
+from .constants import CHAR_TO_HTML_LBS, HTML_LBS_TO_CHAR
+
 
 def get_file_paths(input_dir: str):
     if os.path.isfile(input_dir):
@@ -15,3 +18,13 @@ def get_file_paths(input_dir: str):
     else:
         raise FileNotFoundError("Input file does not exist!")
     return file_list
+
+
+def map_doi_to_filename(doi):
+    name = substring_mapping(doi, CHAR_TO_HTML_LBS)
+    return name
+
+
+def map_filename_to_doi(filename):
+    doi = substring_mapping(filename, HTML_LBS_TO_CHAR)
+    return doi
